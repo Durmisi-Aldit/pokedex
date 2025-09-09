becdbb7bde67cfd25a9dd8d1af86073613d678bf
Durmisi Aldit <alditdurmisi@gmail.com>
2025-09-09 22:11:57 +0200

    Pokemon Daten (name, bild) von API abrufen und in html laden

--- script.js
+++ script.js
@@ -1,14 +1,58 @@
 "use strict";
 
-const DataBase = "https://pokeapi.co/api/v2/pokemon";
-const limit = 20;
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
+const limit = 5;
 const offset = 0;
+const pokemons = [];
+const pokemonCard = document.getElementById("pokedex");
+
+async function fetchPokemonData() {
+  const response = await fetch(`${BASE_URL}?limit=${limit}&offset=${offset}`);
+  return await response.json();
+}
 
 async function loadPokemonData() {
-  const response = await fetch(DataBase + "?limit=" + limit + "&offset=" + offset);
-  const data = await response.json();
+  pokemonCard.innerHTML = "<p>Lade Pokéde ...</p>";
 
-  console.log(data);
+  try {
+    const data = await fetchPokemonData();
+    const pokemons = await getPokemons(data);
+    pokemonCard.innerHTML = template(pokemons);
+  } catch (error) {
+    pokemonCard.innerHTML = "<p style='color:red'>Fehler beim Laden!</p>";
+    console.error(error);
+  }
+}
+
+async function getPokemons(data) {
+  for (const item of data.results) {
+    const res = await fetch(item.url);
+    const details = await res.json();
+
+    pokemons.push({
+      name: details.name,
+      image: details.sprites.front_default,
+    });
+  }
+
+  return pokemons;
+}
+
+function template(pokemons) {
+  return `
+    <ul>
+      ${pokemons
+        .map(
+          (p) => `
+        <li>
+          <img src="${p.image}" alt="${p.name}">
+          ${p.name}
+        </li>
+      `
+        )
+        .join("")}
+    </ul>
+  `;
 }
 
 loadPokemonData();
