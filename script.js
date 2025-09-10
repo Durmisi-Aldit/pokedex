acfef02fd494c24df4ab5f1f7bc19792fac624e8
Durmisi Aldit <alditdurmisi@gmail.com>
2025-09-10 11:43:29 +0200

    style: Pokédex-Karte angefangen, Bilder hochgeladen, Header + Logo gestaltet

--- script.js
+++ script.js
@@ -39,20 +39,40 @@ async function getPokemons(data) {
 }
 
 function template(pokemons) {
-  return `
-    <ul>
-      ${pokemons
-        .map(
-          (p) => `
-        <li>
-          <img src="${p.image}" alt="${p.name}">
-          ${p.name}
-        </li>
-      `
-        )
-        .join("")}
-    </ul>
-  `;
+  return pokemons
+    .map(
+      (pokemon) => `
+  <article class="pkm_card type_grass" aria-labelledby="Pokemon Karte von ${pokemon.name} mit" aria-describedby="Pokemon Karte von ${pokemon.name}">
+    <div class="pkm_container">
+      <header class="pkm_header">
+        <div class="pkm_header_left">
+          <div class="pkm_stage">
+            <span>Level</span>
+          </div>
+          <h2 class="pkm_name">${pokemon.name}</h2>
+        </div>
+
+        <div class="pkm_header_right">
+          <div class="pkm_hit_points">
+            <abbr class="pkm_hit_points_title" title="Hit Points">HP</abbr>
+            <data class="pkm_hit_points_value" value="45">45</data>
+          </div>
+
+          <ul class="pkm_types" aria-label="Typen">
+            <li class="pkm_type color_grass">
+              <img src="https://dummyimage.com/12" width="12" height="12" alt="Pflanze" />
+            </li>
+            <li class="pkm_type color_poison">
+              <img src="https://dummyimage.com/12" width="12" height="12" alt="Gift" />
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  </article>  
+`
+    )
+    .join("");
 }
 
 loadPokemonData();
